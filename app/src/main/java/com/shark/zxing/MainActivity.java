10c9c9372eed7fdf68c0a2ed96902c7e5c6a4d4d
renyuxiang <renyuxiangihealthlabs.com.cn>
2016-12-12 17:50:32 +0800

    精简类库

--- app/src/main/java/com/shark/zxing/MainActivity.java
+++ app/src/main/java/com/shark/zxing/MainActivity.java
@@ -1,13 +1,37 @@
 package com.shark.zxing;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends Activity {
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextView = (TextView) findViewById(R.id.result);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MainActivity.this, CaptureActivity.class), 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == CaptureActivity.SCAN_RESULT_CODE) {
+            resultTextView.setText(data.getStringExtra(CaptureActivity.SCAN_CONTENT_KEY));
+        }
+
     }
 }
